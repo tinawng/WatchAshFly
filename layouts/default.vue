b322af8b6e85d7c9e376cb5ac5780f983b92df6f
Tina Wang <fabzidentiste@gmail.com>
2021-11-23 17:52:07 +0100

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="layout__container">
+    <Nuxt />
+  </div>
+</template>
+
+<style lang="postcss">
+#__layout {
+  @apply min-h-screen;
+  max-width: 64rem;
+  @apply mx-auto;
+}
+</style>
