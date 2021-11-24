c930fef425c2d4ecb35778fde7565fbb9c6e41de
Tina Wang <tina@tina.cafe>
2021-11-24 17:20:09 +0100

    💫 Add letters slide animation

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="page__container">
-    <!-- <section class="ash_price">
-      <h1>Watch the ASH fly</h1>
-      <span class="h1">${{ash_price.toFixed(3)}}</span>
-    </section> -->
+    <section class="ash_price">
+      <h1>${{ash_price.toFixed(3)}}</h1>
+      <div class="overflow-hidden">
+        <span ref="title" class="text-8xl tracking-tighter">Watch&nbsp;the&nbsp;ASH&nbsp;fly</span>
+      </div>
+    </section>
     <section class="cards">
       <card
         style="background: linear-gradient(135deg, #00FABA 0%, #00D8FF 100%);"
@@ -13,7 +15,11 @@
         <ol>
           <li class="mb-2 text-lg">Nifty Gateway</li>
           <li class="text-xs opacity-30">Cube floor</li>
-          <li>$39,00</li>
+          <li>
+            ${{numberWithCommas(nifty_floor.usd)}}
+            <span class="text-xs tracking-wide opacity-40">/ {{nifty_floor.eth}}</span>
+            <span class="opacity-40">♦</span>
+          </li>
         </ol>
       </card>
       <card
@@ -25,7 +31,11 @@
         <ol>
           <li class="mb-2 text-lg">Opensea Gateway</li>
           <li class="text-xs opacity-30">Cube floor</li>
-          <li>$39,00</li>
+          <li>
+            ${{numberWithCommas(opensea_floor.usd)}}
+            <span class="text-xs tracking-wide opacity-40">/ {{opensea_floor.eth}}</span>
+            <span class="opacity-40">♦</span>
+          </li>
         </ol>
       </card>
       <card
@@ -53,18 +63,35 @@ export default {
   components: { Card },
   async fetch() {
     let data = (await this.$http.$post("/", process.env.DATA_API + "/ashStats")).data;
-
-    this.eth_price = parseFloat(data.bundle.ethPriceUSD);
+    this.eth_price = 4241.755785657359203069158769979819;
+    // this.eth_price = parseFloat(data.bundle.ethPriceUSD);
     // this.ash_price = (this.eth_price * parseFloat(data.token.derivedETH)).toFixed(3);
     this.setAshPrice(this.eth_price * parseFloat(data.token.derivedETH));
+
+    this.nifty_floor = (await this.$http.$post("/", process.env.DATA_API + "/niftyfloorStats")).price_in_cents / 100;
+    this.opensea_floor =
+      (await this.$http.$post("/", process.env.DATA_API + "/osfloorStats")).floor_price * this.eth_price;
   },
   data: () => ({
-    eth_price: 0,
-    ash_price: 40,
+    eth_price: 4241.755785657359203069158769979819,
+    ash_price: 47.139,
+
+    nifty_floor: { usd: 39000, eth: 9.18 },
+    opensea_floor: { usd: 37389, eth: 8.8 },
   }),
 
   created() {
     //   setTimeout(this.fetch)
+  },
+
+  mounted() {
+    let el = this.$refs["title"];
+    el.innerHTML = el.textContent.replace(/\w/g, '<span class="slide" style="--delay: #s">$&</span>');
+    var spans = el.innerHTML.split("#");
+    for (let i = 0; i < spans.length - 1; i++) {
+      spans[i] = spans[i] + i * 0.06;
+    }
+    el.innerHTML = spans.join("");
   },
 
   methods: {
@@ -84,22 +111,28 @@ export default {
         }
       }, stepTime);
     },
+
+    numberWithCommas(num) {
+      return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    },
   },
 };
 </script>
 
 <style lang="postcss">
 section.ash_price {
-  @apply h-screen w-2/3;
+  height: 100vh;
+  @apply max-w-7xl;
+  @apply mx-auto;
   @apply pl-12;
-  @apply flex flex-col justify-center;
-  @apply bg-dark text-white;
+  @apply flex flex-col justify-center text-left;
 }
 section.cards {
   height: 80vh;
-  @apply w-full;
+  @apply w-full max-w-6xl;
+  @apply mx-auto;
   @apply bg-white;
-  @apply flex justify-center items-center gap-8;
+  @apply flex justify-between items-center;
 }
 section.ash_stats {
   height: 80vh;
@@ -109,5 +142,19 @@ section.ash_stats {
 }
 section.credits {
   @apply h-32 w-full;
+}
+
+.slide {
+  display: inline-block;
+  transform: translateY(2ch);
+  animation: slide 1s ease-in-out var(--delay) forwards;
+}
+@keyframes slide {
+  from {
+    transform: translateY(2ch);
+  }
+  to {
+    transform: translateY(0);
+  }
 }
 </style>
